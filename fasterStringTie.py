e94fcdbe2608c8efafa953ccae32bc70daf487e4
Waqar Hanif <66572358+waqarhanif-biocode@users.noreply.github.com>
2023-08-31 15:39:26 +0500

    commit-final

--- fasterStringTie.py
+++ fasterStringTie.py
@@ -1,24 +1,46 @@
 import os
+import subprocess
 import multiprocessing
+import datetime
+import argparse
+
+current_datetime = datetime.datetime.now()
+current_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+# Check if StringTie is installed
+try:
+    subprocess.run(["stringtie", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+except subprocess.CalledProcessError:
+    print("Error: StringTie is not installed. Please install StringTie and make sure it's in your PATH.")
+    exit(1)
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Faster Transcriptome Assembly of BAM files using StringTie.")
+parser.add_argument("accessions_file", help="Path to the accessions.txt file")
+parser.add_argument("-i", "--input_dir", help="Directory where the BAM files are located")
+parser.add_argument("-o", "--output_dir", help="Output directory")
+parser.add_argument("-r", "--reference", help="Path to the reference GTF file")
+args = parser.parse_args()
+
+accessions_file = args.accessions_file
+input_dir = args.input_dir
+output_dir = args.output_dir
+reference = args.reference
 
 def process_accession(accession):
     command = (
-        f"{inst_dir}{cores}-G {reference} "
-        f"{input_dir}{accession}_dedup.bam -o {output_dir}{accession}.gtf"
+        f"stringtie {cores} {reference} "
+        f"{os.path.join(input_dir, accession)}.bam -o {os.path.join(output_dir, accession)}.gtf"
     )
-    print(f"{command} ....running....")
+    print(f"{accession} ....running....")
     os.system(command)
 
 if __name__ == "__main__":
-    file = open("accessions.txt")
-    accessions = file.read().split()
+    with open(accessions_file, "r") as file:
+        accessions = file.read().split()
 
-    inst_dir = "stringtie"
-    reference = "/opt/Homo_sapiens.GRCh38.108.gtf"
-    input_dir = "/media/waqar/hdd/easeRNAseq/E-MTAB-8412_colorectal_border/mapping/bam_sorted/"
-    output_dir = "/home/waqar/Desktop/test-string/"
 
-    cores = " -p 12 "
+    cores = " -p 8 "
 
     num_cores = multiprocessing.cpu_count()
     max_cores_to_use = int(num_cores * 0.75)  # Limit to 75% of available cores
@@ -26,4 +48,9 @@ if __name__ == "__main__":
     with multiprocessing.Pool(processes=max_cores_to_use) as pool:
         pool.map(process_accession, accessions)
 
+    
+    print("Started at: " + current_datetime)
+    current_datetime = datetime.datetime.now()
+    endtime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    print("Finised at: " + endtime)
     print("All done")
